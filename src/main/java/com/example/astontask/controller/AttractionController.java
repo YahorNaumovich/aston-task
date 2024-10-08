be6013a8dfee4c09cf52bcd4b6a1611fba84a34c
YahorNaumovich <egor.n.naumovich@gmail.com>
2024-10-08 19:58:45 +0300

    Added empty controllers

---
+++ src/main/java/com/example/astontask/controller/AttractionController.java
@@ -0,0 +1,11 @@
+package com.example.astontask.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/attractions")
+@RequiredArgsConstructor
+public class AttractionController {
+}
