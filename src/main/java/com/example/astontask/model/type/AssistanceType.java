79ef1c016ebdded9b50d0208595c035c2bec326a
YahorNaumovich <egor.n.naumovich@gmail.com>
2024-10-09 18:34:49 +0300

    Added javadoc

--- src/main/java/com/example/astontask/model/type/AssistanceType.java
+++ src/main/java/com/example/astontask/model/type/AssistanceType.java
@@ -1,5 +1,8 @@
 package com.example.astontask.model.type;
 
+/**
+ * Enum representing the different types of assistance available.
+ */
 public enum AssistanceType {
     GUIDE, CAR_TOUR, FOOD
 }
--- src/main/java/com/example/astontask/model/type/AttractionType.java
+++ src/main/java/com/example/astontask/model/type/AttractionType.java
@@ -1,5 +1,8 @@
 package com.example.astontask.model.type;
 
+/**
+ * Enum representing the different types of attractions.
+ */
 public enum AttractionType {
     PALACES, PARKS, MUSEUMS, ARCHEOLOGICAL_SITES, RESERVES
 }
