ee481fda3c2d43f51c02e8bf06201ba4ff15a94f
YahorNaumovich <egor.n.naumovich@gmail.com>
2024-10-09 14:24:04 +0300

    Added global exception handler

---
+++ src/main/java/com/example/astontask/controller/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.astontask.controller;
+
+import com.example.astontask.exception.EntityNotFoundException;
+import com.example.astontask.exception.InvalidDataException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidDataException.class)
+    public ResponseEntity<String> handleInvalidDataException(InvalidDataException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
